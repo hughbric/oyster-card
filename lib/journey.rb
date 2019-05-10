6e986e41f36ed23819ebf8c605d098942f407e45
Michael Hughes <mhughes36@live.co.uk>
2019-05-10 23:39:15 +0100

    Journey checks penalty fare; has a finish station method

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,22 @@
 class Journey
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
+  # MINUMUM_FARE = 1
+  PENALTY_FARE = 6
 
   def initialize(station = nil)
     @entry_station = station
   end
 
-  def in_journey?
+  def complete?
     !!@entry_station
+  end
+
+  def fare
+    PENALTY_FARE
+  end
+
+  def finish(station)
+    @exit_station = station
+    itself
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,18 +1,32 @@
 require 'journey'
 
 describe Journey do
+let(:station) { double :station, zone: 1}
+
+  it 'knows if a journey is not complete' do
+    expect(subject).not_to be_complete
+  end
+
+  it 'has a penalty fare by default' do
+    expect(subject.fare).to eq Journey::PENALTY_FARE
+  end
+
+  it "returns itself when exiting a journey" do
+    expect(subject.finish(station)).to eq(subject)
+  end
+
   context do
     before(:each) do
-      @station = double :station
-      @new_journey = Journey.new(@station)
-    end
-
-    it 'is initially not in a journey' do
-      expect(subject).not_to be_in_journey
+      @new_journey = Journey.new(station)
     end
 
     it 'saves the entry station when starting a new journey' do
-      expect(@new_journey.entry_station).to eq(@station)
+      expect(@new_journey.entry_station).to eq(station)
     end
+
+    # it 'saves the exit station when end_journey is called' do
+    #   @new_journey.end_journey(@station)
+    #   expect(@new_journey.exit).to eq(@station)
+    # end
   end
 end
